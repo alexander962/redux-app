880ddc67dce8e414df94a82af4f9b337ffec1789
alexander962 <sanek12011996@gmail.com>
2025-05-13 08:01:30 +0300

    add structure and new reducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
 
 function App() {
+  // чтобы как то изменить состояние нужен dispatch
+  const dispatch = useDispatch();
+  // получаем состояние
+  const cash = useSelector((state) => state.cash.cash);
+
+  const addCash = (cash) => {
+    dispatch({type: 'ADD_CASH', payload: cash});
+  }
+
+  const getCash = (cash) => {
+    dispatch({type: 'GET_CASH', payload: cash});
+  }
+
   return (
     <div className="App">
-      App
+      <div style={{fontSize: '40px'}}>{cash}</div>
+      <div className="buttons-block">
+        <button onClick={() => addCash(Number(prompt()))}>Пополнить счет</button>
+        <button onClick={() => getCash(Number(prompt()))}>Снять со счета</button>
+      </div>
     </div>
   );
 }
