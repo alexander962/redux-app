f122e9eafa43b0654c9dd68627d7135958ba93cd
alexander962 <sanek12011996@gmail.com>
2025-05-19 08:07:34 +0300

    fix

--- src/saga/index.js
+++
@@ -1,8 +0,0 @@
-import {all} from 'redux-saga/effects';
-import {countWatcher} from "./countSaga";
-import {userWatcher} from "./userSaga";
-
-export function* rootWatcher() {
-  // all - позволяет объединить несколько вотчеров
-  yield all([countWatcher(), userWatcher()])
-}
