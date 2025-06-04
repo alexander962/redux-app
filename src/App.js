277a08abd9c5ae87d9c9b9c6a78b9db65ee11010
alexander962 <sanek12011996@gmail.com>
2025-06-04 14:01:58 +0300

    add fetch users with redux-toolkit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import {useAppDispatch, useAppSelector} from "./hooks/redux";
+import {userSlice} from "./store/reducers/UserSlice";
+import {useEffect} from "react";
+import {fetchUsers} from "./store/reducers/ActionCreators";
 
 function App() {
+  const dispatch = useAppDispatch();
+  const {users, isLoading, error} = useAppSelector(state => state.userReducer);
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
   return (
     <div className="App">
-      Hello
+      {isLoading && <h1>Loading...</h1>}
+      {error && <h1>Error: {error}</h1>}
+      {JSON.stringify(users, null, 2)}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
+import {Provider} from "react-redux";
+import {setupStore} from "./store/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const store = setupStore();
+
+ReactDOM.render(
+  <Provider store={store}>
     <App />
+  </Provider>,
+  document.getElementById('root')
 );
